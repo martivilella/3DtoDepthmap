628227e2808684d845feaacdd4e5d9d2d37f193a
Marti Vilella <mvilellar@outlook.com>
2021-06-10 19:17:15 +0200

    refactor camera object creation into separate fcn, misc renaming of vars

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ import os
 import sys
 import png
 
+def create_camera(w, h, fx, fy, cx, cy, extr):
+    '''
+    Create [open3d.camera.PinholeCameraParameters](http://www.open3d.org/docs/release/python_api/open3d.camera.PinholeCameraParameters.html#open3d.camera.PinholeCameraParameters) object from pinhole camera parameters and extrinsic 4x4 numpy array
+    '''
+    cam_params=o3d.camera.PinholeCameraParameters()
+    cam_params.intrinsic.set_intrinsics(w, h, fx, fy, cx, cy)
+    cam_params.extrinsic=extr
+    return cam_params
+
 def create_visualiser(mesh, camera_parameters):
     '''
     Create [open3d.visualization.Visualizer](http://www.open3d.org/docs/release/python_api/open3d.visualization.Visualizer.html#open3d.visualization.Visualizer) object from geometry and intr+extr camera params
@@ -25,7 +34,6 @@ def write_intr_file(filename, intr_params):
     intr_file.write(f'{{"_cy": {cxy[1]:f}, "_cx": {cxy[0]:f}, "_fy": {fxy[1]:f}, "_height": {intr_params.height}, "_fx": {fxy[1]:f}, "_width": {intr_params.width}, "_skew": {intr_params.get_skew():f}, "_K": {0}, "_frame": "{fname}"}}') # no distortion
     intr_file.close()
     
-
 def main(inpath):
     # set in/out paths
     filepath,fullflname = os.path.split(inpath)
@@ -51,11 +59,9 @@ def main(inpath):
     w=1000
     h=1000
     f=3000
-    cam_params=o3d.camera.PinholeCameraParameters()
-    cam_params.intrinsic.set_intrinsics(w, h, f, f, w/2.0-0.5, h/2.0-0.5) # TODO Adjust
-    extr = np.identity(4,dtype=np.float64) # TODO Adjust
+    extr = np.identity(4,dtype=np.float64)
     extr[2,3] = 100
-    cam_params.extrinsic=extr
+    cam_params = create_camera(w, h, f, f, w/2.0-0.5, h/2.0-0.5, extr)
     
     # create open3d visualiser
     vis = create_visualiser(mesh, cam_params)
@@ -66,12 +72,12 @@ def main(inpath):
     depth = vis.capture_depth_float_buffer(True)
     depth2darray = np.asarray(depth) # Depth in mm
     depthsize = np.shape(depth2darray)
-    deptharray = np.reshape(depth2darray, (depthsize[0], depthsize[1], 1)) # reshape to 3D array
-    #print(f"max={np.max(depth2darray):f}, min={np.min(depth2darray):f}")
+    depth3darray = np.reshape(depth2darray, (depthsize[0], depthsize[1], 1)) # reshape to 3D array
+    print(f"Depth map stats: \n max: {np.max(depth2darray):f}\tmm\n min: {np.min(depth2darray[depth2darray.astype(bool)]):.5f}\tmm") # apply mask to min calculation, background has value of 0
     
     # save outputs
     ## npy depth array
-    np.save(outdeptharraypath, deptharray)
+    np.save(outdeptharraypath, depth3darray)
 
     ## png depth map
     png.from_array(depth2darray.astype(np.uint8), mode="L").save(outdepthpath)
