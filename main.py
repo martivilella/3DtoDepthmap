3483c04f1e29ba395518e16301ebbd51d07560c0
Marti Vilella <mvilellar@outlook.com>
2021-06-10 17:54:27 +0200

    refactor code for readibility

--- main.py
+++ main.py
@@ -5,11 +5,26 @@ import sys
 import cv2 as cv
 
 def create_visualiser(mesh, camera_parameters):
+    '''
+    Create [open3d.visualization.Visualizer](http://www.open3d.org/docs/release/python_api/open3d.visualization.Visualizer.html#open3d.visualization.Visualizer) object from geometry and intr+extr camera params
+    '''
     vis = o3d.visualization.Visualizer()
     vis.create_window()
     vis.add_geometry(mesh)
     vis.get_view_control().convert_from_pinhole_camera_parameters(camera_parameters, True) # allow arbitrary camera width/height
     return vis
+
+def write_intr_file(filename, intr_params):
+    '''
+    Generate an intrinsic camera parameters file with the format used by the GQCNN project by UC Berkeley
+    '''
+    fname, ext = os.path.splitext(filename)
+    fxy = intr_params.get_focal_length()
+    cxy = intr_params.get_principal_point()
+    intr_file = open(filename, "w")
+    intr_file.write(f'{{"_cy": {cxy[1]:f}, "_cx": {cxy[0]:f}, "_fy": {fxy[1]:f}, "_height": {intr_params.height}, "_fx": {fxy[1]:f}, "_width": {intr_params.width}, "_skew": {intr_params.get_skew():f}, "_K": {0}, "_frame": "{fname}"}}') # no distortion
+    intr_file.close()
+    
 
 def main(inpath):
     # set in/out paths
@@ -19,11 +34,12 @@ def main(inpath):
     outdeptharraypath=os.path.dirname(filepath)+'depthnpy/'+fname+'.npy'
     outsegmaskpath=os.path.dirname(filepath)+'segmask/'+fname+'.png'
 
-    # load mesh
+    # load properties
+    ## mesh
     mesh = o3d.io.read_triangle_mesh(inpath)
     mesh.compute_vertex_normals()
-
-    # load camera params
+    
+    ## camera params
     w=1000
     h=1000
     f=3000
@@ -33,32 +49,31 @@ def main(inpath):
     extr[2,3] = 100
     cam_params.extrinsic=extr
     
+    # create open3d visualiser
     vis = create_visualiser(mesh, cam_params)
-    
-    # visualise obj
     #vis.run()
     #vis.destroy_window()
 
     # capture depth img
     depth = vis.capture_depth_float_buffer(True)
-    deptharray = np.asarray(depth) # Depth in mm
-    print(f"max={np.max(np.asarray(depth)):f}, min={np.min(np.asarray(depth)):f}")
+    depth2darray = np.asarray(depth) # Depth in mm
+    depthsize = np.shape(depth2darray)
+    deptharray = np.reshape(depth2darray, (depthsize[0], depthsize[1], 1)) # reshape to 3D array
+    #print(f"max={np.max(depth2darray):f}, min={np.min(depth2darray):f}")
     
     # save outputs
     ## npy depth array
-    np.save(outdeptharraypath, deptharray) 
+    np.save(outdeptharraypath, deptharray)
+
     ## png depth map
     cv.imwrite(outdepthpath,np.round(deptharray*1000).astype(np.uint16))
+
     ## png segmask
     cv.imwrite(outsegmaskpath, deptharray.astype(bool)*255)
-
-    # save intrinsic camera params in UC Berkeley GQCNN format
-    fxy = cam_params.intrinsic.get_focal_length()
-    cxy = cam_params.intrinsic.get_principal_point()
-    intr_file = open("virtualcam.intr", "w")
-    intr_file.write(f'{{"_cy": {cxy[1]:f}, "_cx": {cxy[0]:f}, "_fy": {fxy[1]:f}, "_height": {cam_params.intrinsic.height}, "_fx": {fxy[1]:f}, "_width": {cam_params.intrinsic.width}, "_skew": {cam_params.intrinsic.get_skew():f}, "_K": {0}, "_frame": "virtualcam"}}') # no distortion
-    intr_file.close()
     
+    ## intrinsic camera params
+    write_intr_file("virtualcam.intr", cam_params.intrinsic)
+
 if __name__ == "__main__":
     if (len(sys.argv) != 2):
         print("Provide exactly one CLI arg as the relative path to the 3D model to load\n")
