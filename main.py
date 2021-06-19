fix(depthImage): clip array to avoid overflow

main.py
@@ -85,7 +85,7 @@ def main(inpath, s=1000.0):
     png.from_array((depth2darray*float(s)).astype(np.uint8), mode="L").save(outdepthpath)
 
     ## png segmask
-    png.from_array((depth2darray.astype(bool)*255).astype(np.uint8), mode="L").save(outsegmaskpath)
+    png.from_array(np.clip(depth2darray.astype(bool)*255, 0, 255).astype(np.uint8), mode="L").save(outsegmaskpath)
     
     ## intrinsic camera params
     write_intr_file("virtualcam.intr", cam_params.intrinsic)